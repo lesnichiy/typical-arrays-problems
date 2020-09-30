812bb4da499726e2ef8afc0ea22d8d413bf9948e
lesnichiy <lesnichiy02@ya.ru>
2020-09-30 21:05:35 +0300

    feat: add a solution of the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+    if ( array === undefined || !array.length) {
+        return 0;
+    } else {
+        return Math.min(...array);
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ( array === undefined || !array.length) {
+        return 0;
+    } else {
+        return Math.max(...array);
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ( array === undefined || !array.length) {
+        return 0;
+    } else {
+        return array.reduce( (sum, item) => sum + item, 0) / array.length;
+    }
 }
